4c04102e71b195d6048cda1ae7e2100e1846b6e5
Ryan <ryanwscott@gmail.com>
2013-10-12 20:34:07 -0400

    Merge fuzzbot branch into master

---
+++ music-web/src/test/java/musiclibrary/impl/web/controller/SpringContextTest.java
@@ -0,0 +1,18 @@
+package musiclibrary.impl.web.controller;
+
+import org.apache.log4j.Logger;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@ContextConfiguration(locations = { "classpath:test-music-application.xml" })
+@RunWith(SpringJUnit4ClassRunner.class)
+public class SpringContextTest {
+	private static final Logger LOG = Logger.getLogger(SpringContextTest.class);
+
+	@Test
+	public void testContext() {
+		LOG.info("Loaded applciation context");
+	}
+}
---
+++ music-web/src/test/resources/db/data.sql
@@ -0,0 +1,1 @@
+INSERT INTO MUSIC (SONG) VALUES ('Simple Song Title');
