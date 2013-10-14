5684561006c1495d25502676ac246ee60d78e950
Meghan <ericksonDOTmeghan@gmail.com>
2013-10-14 13:34:23 -0400

    added jQuery jPlayer on client. Added basic mp3 streaming using Apache IOUtils in Spring controller

--- music-web/src/main/java/musiclibrary/impl/web/controller/EntryController.java
+++ music-web/src/main/java/musiclibrary/impl/web/controller/EntryController.java
@@ -1,8 +1,19 @@
 package musiclibrary.impl.web.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.IOUtils;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.mysql.jdbc.log.Log;
 
 @Controller
 public class EntryController {
@@ -15,5 +26,27 @@ public class EntryController {
 		m.addObject("model", "hey this is meghan and i miss tucker."); 
 		
 		return m;
+	}
+	
+	@RequestMapping("/play/*.mp3")
+	public void playSong( HttpServletResponse response) throws IOException{
+			response.setContentType("audio/mpeg");
+			try{
+			ServletOutputStream os = response.getOutputStream();
+			
+			ClassPathResource resource = new ClassPathResource("mp3/jolene.mp3");
+			File mp3 = resource.getFile();
+			//response.setContentLengthLong(mp3.length());
+			//response.setContentLengthLong(len);
+			//response.setHeader( "Content-Disposition", "attachment;filename=" + mp3 );
+			
+			InputStream is = resource.getInputStream();
+			IOUtils.copy(is, os);
+			response.flushBuffer(); //forces stream to be written to client
+			
+			}catch(IOException e){
+				throw new RuntimeException("IOError writing file to output stream");
+			}
+			
 	}
 }
--- music-web/src/test/java/musiclibrary/impl/web/controller/SpringContextTest.java
+++ music-web/src/test/java/musiclibrary/impl/web/controller/SpringContextTest.java
@@ -1,4 +1,4 @@
-package musiclibrary.impl.web.controller;
+package src.test.java.musiclibrary.impl.web.controller;
 
 import org.apache.log4j.Logger;
 import org.junit.Test;
