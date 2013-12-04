e7b6c4813cfc1e386fc1bd508e696fe20854e706
Ryan Scott <ryanwscott@gmail.com>
2013-12-04 11:20:06 -0500

    Added maven plugin for jsLint validation of JavaScript

--- music-web/src/main/webapp/public/assets/js/tuckishplayer.js
+++ music-web/src/main/webapp/public/assets/js/tuckishplayer.js
@@ -1,8 +1,7 @@
-<script type="text/javascript">
 //instance jplayer here
 //use ajax to get a new mp3 and set the new mp3 in this jplayers instance
 
-$(document).ready(function(){
+$(document).ready(function() {
 	var ffaction;
 	var rwaction;
 	var rewinding = false;
@@ -18,67 +17,62 @@ $(document).ready(function(){
 		supplied: "mp3"
 	});
 	
- $(document).delegate('*', 'keydown',  function (e){
-	 //Rewind
-	if(e.keyCode == 37 && (!rewinding)){ //left arrow key pressed
-		rewinding = true;
-		$("#jquery_jplayer_1").jPlayer("pause");
-		rewind();
-		rwaction = window.setInterval(function(){rewind()}, 500);
-	}
-	else if(e.keyCode == 39 && (!fastforwarding)){//right arrow key pressed
-		fastforwarding = true;
-		$("#jquery_jplayer_1").jPlayer("pause");
-		fastforward();
-		ffaction = window.setInterval(function(){fastforward()}, 500);
-	}
- });
-
- $(document).delegate('*', 'keyup',  function (e){
-	 //stop Rewind
-	if(e.keyCode == 37){ //left arrow key up
-		rewinding = false;
-		window.clearInterval(rwaction);
-		$("#jquery_jplayer_1").jPlayer("pause");
-		
-	}
-	else if(e.keyCode == 39){//right arrow key up
-		//stop ff
-		fastforwarding = false;
-		window.clearInterval(ffaction);
-		$("#jquery_jplayer_1").jPlayer("pause");
-    }
- });
- 
- function getPlayerProgress(){
-	 var toReturn = ($('.jp-play-bar').width() / $('.jp-seek-bar').width()) * 100;
-	 return( toReturn);
-	 //width() sets or returns width of selected element; not including padding, border, margin
- }
+	$(document).delegate('*', 'keydown',  function (e) {
+		//Rewind
+		if(e.keyCode == 37 && (!rewinding)) { //left arrow key pressed
+			rewinding = true;
+			$("#jquery_jplayer_1").jPlayer("pause");
+			rewind();
+			rwaction = window.setInterval(function(){rewind();}, 500);
+		} else if (e.keyCode == 39 && (!fastforwarding)) { //right arrow key pressed
+			fastforwarding = true;
+			$("#jquery_jplayer_1").jPlayer("pause");
+			fastforward();
+			ffaction = window.setInterval(function(){fastforward();}, 500);
+		}
+	});
 	
- function rewind(){
-	 var progress = getPlayerProgress();
-	 var futureprogress = progress - 10;
-	 if(futureprogress<=10){
-		 rewinding = false;
-		 window.clearInterval(rwaction);
-		 $("#jquery_jplayer_1").jPlayer("pause", 0);
-	 }else{
-		 $("#jquery_jplayer_1").jPlayer("playHead", futureprogress);
-	 }
- }
- 
- function fastforward(){
-	 var progress = getPlayerProgress();
-	 var futureprogress = progress + 10;
-	 if (futureprogress >= 100){
-		 fastforwarding=false;
-		 window.clearIntervale(ffaction);
-		 $("#jquery_jplayer_1").jPlayer("pause", 0);
-	 }else{
-		 $("#jquery_jplayer_1").jPlayer("playHead", futureprogress);
-	 }
- }
- 
+	$(document).delegate('*', 'keyup',  function (e) {
+		//stop Rewind
+		if (e.keyCode == 37) { //left arrow key up
+			rewinding = false;
+			window.clearInterval(rwaction);
+			$("#jquery_jplayer_1").jPlayer("pause");
+		} else if (e.keyCode == 39) {//right arrow key up
+			//stop ff
+			fastforwarding = false;
+			window.clearInterval(ffaction);
+			$("#jquery_jplayer_1").jPlayer("pause");
+		}
+	});
+	
+	function getPlayerProgress() {
+		var toReturn = ($('.jp-play-bar').width() / $('.jp-seek-bar').width()) * 100;
+		return(toReturn);
+		//width() sets or returns width of selected element; not including padding, border, margin
+	}
+	
+	function rewind() {
+		var progress = getPlayerProgress();
+		var futureprogress = progress - 10;
+		if (futureprogress<=10) {
+			rewinding = false;
+			window.clearInterval(rwaction);
+			$("#jquery_jplayer_1").jPlayer("pause", 0);
+		} else {
+			$("#jquery_jplayer_1").jPlayer("playHead", futureprogress);
+		}
+	}
+	
+	function fastforward() {
+		var progress = getPlayerProgress();
+		var futureprogress = progress + 10;
+		if (futureprogress >= 100) {
+			fastforwarding = false;
+			window.clearIntervale(ffaction);
+			$("#jquery_jplayer_1").jPlayer("pause", 0);
+		} else {
+			$("#jquery_jplayer_1").jPlayer("playHead", futureprogress);
+		}
+	}
 });
-</script>
