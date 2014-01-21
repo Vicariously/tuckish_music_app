7ef80a26bdfee9f6c8760ff63b98625a02a77069
Meghan <ericksonDOTmeghan@gmail.com>
2014-01-21 18:38:43 -0500

    dont know why this didnt get committed

--- music-common/src/main/java/musiclibrary/impl/common/service/OrganizeService.java
+++ music-common/src/main/java/musiclibrary/impl/common/service/OrganizeService.java
@@ -2,6 +2,9 @@ package musiclibrary.impl.common.service;
 
 import musiclibrary.impl.common.organizor.OrganizeContext;
 import musiclibrary.impl.common.organizor.Stash;
+import musiclibrary.impl.common.organizor.strategy.OrganizeAlbum;
+import musiclibrary.impl.common.organizor.strategy.OrganizeArtist;
+import musiclibrary.impl.common.organizor.strategy.OrganizeMp3;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,7 +18,18 @@ public class OrganizeService {
 	@Autowired
 	private OrganizeContext organizeContext;
 	
-	public Stash organize(){
+	public Stash organize(String fileName, String fileType){
+		
+		if(fileType.equals("artist")){
+			organizeContext.setOrganizer(new OrganizeArtist());
+		}else if(fileType.equals("album")){
+			organizeContext.setOrganizer(new OrganizeAlbum());
+		}else if(fileType.equals("song")){
+			organizeContext.setOrganizer(new OrganizeMp3());
+		}
+		organizeContext.organize(fileName, fileType);
+		
+		
 		return new Stash();
 	}
 }
