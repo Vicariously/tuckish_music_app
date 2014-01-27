483070a6cdcb689b39fc4d34f6ff5460b9628b83
Ryan Scott <ryanwscott@gmail.com>
2014-01-27 11:36:14 -0500

    resolves #1 Updated parent SQL script to use BIGINT data types. Update also includes demo of collapsed creation of FKs in table definition.

--- sql/setup_music.sql
+++ sql/setup_music.sql
@@ -1,4 +1,4 @@
-set foreign_key_checks=0;
+SET foreign_key_checks = 0;
 
 DROP TABLE IF EXISTS song;
 DROP TABLE IF EXISTS album;
@@ -8,30 +8,54 @@ DROP TABLE IF EXISTS genre;
 -- ---------------------------------------
 -- ---------------------------------------
 
-CREATE TABLE IF NOT EXISTS song( id INT NOT NULL AUTO_INCREMENT, title VARCHAR(30), track TINYINT, comment VARCHAR(50), type VARCHAR(15), filepath VARCHAR(100) NOT NULL, PRIMARY KEY (id) );
+CREATE TABLE IF NOT EXISTS song(
+  id BIGINT NOT NULL AUTO_INCREMENT,
+  album_id BIGINT NOT NULL,
+  title VARCHAR(30),
+  track TINYINT,
+  comment VARCHAR(50),
+  type VARCHAR(15),
+  filepath VARCHAR(100) NOT NULL,
+  PRIMARY KEY (id),
+  CONSTRAINT FK_ALBUM_SONG FOREIGN KEY (album_id) REFERENCES album(id)
+);
 
-CREATE TABLE IF NOT EXISTS album( id INT NOT NULL AUTO_INCREMENT, title VARCHAR(30), PRIMARY KEY (id));
+CREATE TABLE IF NOT EXISTS album(
+  id BIGINT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30),
+  PRIMARY KEY (id)
+);
 
-CREATE TABLE IF NOT EXISTS artist(id INT NOT NULL AUTO_INCREMENT, name VARCHAR(30), PRIMARY KEY(id));
+CREATE TABLE IF NOT EXISTS artist(
+  id BIGINT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30),
+  PRIMARY KEY(id)
+);
 
-CREATE TABLE IF NOT EXISTS genre(id INT NOT NULL AUTO_INCREMENT, name VARCHAR(30), PRIMARY KEY(id));
+CREATE TABLE IF NOT EXISTS genre(
+  id BIGINT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30),
+  PRIMARY KEY(id)
+);
 
 -- ---------------------------------------
 -- ---------------------------------------
-ALTER TABLE song ADD COLUMN album_id INT NOT NULL;
-ALTER TABLE song ADD FOREIGN KEY (album_id) REFERENCES album(id);
-ALTER TABLE song ADD COLUMN artist_id INT NOT NULL;
+-- ALTER TABLE song ADD COLUMN album_id INT NOT NULL;
+-- ALTER TABLE song ADD FOREIGN KEY (album_id) REFERENCES album(id);
+
+ALTER TABLE song ADD COLUMN artist_id BIGINT NOT NULL;
 ALTER TABLE song ADD FOREIGN KEY (artist_id) REFERENCES artist(id);
 
-ALTER TABLE album ADD COLUMN artist_id INT NOT NULL;
+ALTER TABLE album ADD COLUMN artist_id BIGINT NOT NULL;
 ALTER TABLE album ADD FOREIGN KEY (artist_id) REFERENCES artist(id);
 
-ALTER TABLE artist ADD COLUMN genre_id INT NOT NULL;
+ALTER TABLE artist ADD COLUMN genre_id BIGINT NOT NULL;
 ALTER TABLE artist ADD FOREIGN KEY (genre_id) REFERENCES genre(id);
 
 -- ---------------------------------------
 -- ---------------------------------------
 
-set foreign_key_checks=1;
+SET foreign_key_checks = 1;
 
-insert into song(title, track, comment, type, filepath )values('Ends', 2, 'this is a new comment', 'mp3', '/home/meghan/Documents/TestMusicDirectory/Everlast/Whitey Ford Sings the Blues');
+INSERT INTO song(title, track, comment, type, filepath)
+VALUES('Ends', 2, 'this is a new comment', 'mp3', '/home/meghan/Documents/TestMusicDirectory/Everlast/Whitey Ford Sings the Blues');
